c3dfca7a3af8dff9e3f30ca697c7ff344bbf3bef
Anupam Choudhary <87457759+Anupam0-0@users.noreply.github.com>
2024-11-28 11:52:41 +0000

    Added New Projects

---
+++ Pokemon-dex/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import './App.css'
+import Pokemon from './Pokemon'
+
+const App = () => {
+  return (
+    <div>
+      <Pokemon  />
+
+    </div>
+  )
+}
+
+export default App
